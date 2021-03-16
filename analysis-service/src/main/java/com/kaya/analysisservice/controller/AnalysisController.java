6d0d6cb3fd4fbd32852a2769e3b86e32d4836a77
Caner Kaya <canerky96@gmail.com>
2021-03-17 00:12:24 +0300

    processor refactored.

--- analysis-service/src/main/java/com/kaya/analysisservice/controller/AnalysisController.java
+++ analysis-service/src/main/java/com/kaya/analysisservice/controller/AnalysisController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping
 public class AnalysisController {
 
   @GetMapping
-  public Integer getTotalPrice() {
-    return ProductStreamProcessor.count.get();
+  public Map<Integer, Integer> getTotalPrice() {
+    return ProductStreamProcessor.products;
   }
 }
--- analysis-service/src/main/java/com/kaya/analysisservice/processor/ProductStreamProcessor.java
+++ analysis-service/src/main/java/com/kaya/analysisservice/processor/ProductStreamProcessor.java
@@ -7,19 +7,18 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.support.serializer.JsonSerde;
 import org.springframework.stereotype.Component;
 
-import java.time.Duration;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 public class ProductStreamProcessor {
 
-  public static final AtomicInteger count = new AtomicInteger();
+  public static Map<Integer, Integer> products = new HashMap<>();
 
   @Autowired
   public void process(StreamsBuilder builder) {
@@ -28,16 +27,11 @@ public class ProductStreamProcessor {
     Serde<SoldProduct> valueSerde = new JsonSerde<>(SoldProduct.class);
 
     builder.stream("sold-product", Consumed.with(keySerde, valueSerde))
-        .map((key, value) -> new KeyValue<>(value.getId().toString(), value))
-        .groupByKey()
-        .windowedBy(TimeWindows.of(Duration.ofSeconds(10)))
-        .count()
-        .toStream()
-        .map((key, value) -> new KeyValue<>(key.key(), value))
-        .foreach((key, value) -> increase(value.intValue()));
+        .map((key, value) -> new KeyValue<>(value.getId(), value))
+        .foreach(this::add);
   }
 
-  private void increase(Integer value) {
-    count.addAndGet(value);
+  private void add(Integer key, SoldProduct soldProduct) {
+    products.put(key, products.getOrDefault(key, 0) + soldProduct.getPrice());
   }
 }
